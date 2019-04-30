9b402ea5a2f762fa8abb91b49c40b7cc76f8fe61
Petrs911 <petrs911@gmail.com>
2019-04-30 17:14:43 +0300

    V.1

---
+++ what_to_see.py
@@ -0,0 +1,31 @@
+import random
+
+with open ("Films.txt", "r") as list1:
+    film_list = list1.read().split('\n')  
+     
+def movie_search(some_list):
+    while True:
+        film = random.choice(some_list)
+        if '@' in film:
+            i = True
+        else:
+            return film
+            break
+           
+def change_list(some_film):
+    index_element = film_list.index(some_film)
+    film_list[index_element] = some_film + '@'
+    changed_film_list = "\n".join(film_list)
+    return changed_film_list    
+
+a = movie_search(film_list)
+b = change_list(a)
+print('Выпал этот фильм: ', a)
+
+with open ("Films.txt", "w") as film:
+    film.write(b)       
+
+
+
+
+
